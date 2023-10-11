03fce67ae60c66588b256ac9c453a11d737ac35b
Sojourner64564 <122652562+Sojourner64564@users.noreply.github.com>
2023-10-11 06:03:38 +0300

    lets get started

--- lib/features/youtube_comment_finder/presentation/cubit/fetch_comments_cubit.dart
+++ lib/features/youtube_comment_finder/presentation/cubit/fetch_comments_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import 'package:youtube_comment_finder/features/youtube_comment_finder/domain/entitys/comment_thread_entity.dart';
 import 'package:youtube_comment_finder/features/youtube_comment_finder/domain/usecase/params/no_params.dart';
 import 'package:youtube_comment_finder/features/youtube_comment_finder/domain/usecase/use_case_comment_thread_impl.dart';
 part 'fetch_comments_state.dart';
@@ -11,8 +12,15 @@ class FetchCommentsCubit extends Cubit<MyState> {
   final UseCaseCommentThreadImpl useCaseCommentThreadImpl;
 
   void fetchComments() async {
-    final faliureOrCommentsEither = await useCaseCommentThreadImpl.call(NoParams());
-    final falureOrComments = faliureOrCommentsEither.fold((failure) => ErrorState(), (commentThread) => commentThread);
+    final failureOrCommentsEither = await useCaseCommentThreadImpl.call(NoParams());
+    final failureOrComments = failureOrCommentsEither.fold((failure) => ErrorState(), (commentThread) => commentThread);
+    if(failureOrComments is ErrorState){
+      emit(failureOrComments);
+    }
+
+    print((failureOrComments as CommentThreadEntity));
+    //final bruh = (failureOrComments as CommentThreadEntity).items[0].itemSnippet.topLevelComment.
+    //emit(LoadedState(failureOrComments as CommentThreadEntity));
 
   }
 }
